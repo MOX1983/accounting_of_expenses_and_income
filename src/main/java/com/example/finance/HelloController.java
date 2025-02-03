2646db7e76cd09d73e0df3bd2a67b2b6772875e0
Mox <tenmaken246@gmail.com>
2025-02-04 01:11:43 +0300

    графики, добовление и общая сумма доходов и расходов

--- src/main/java/com/example/finance/HelloController.java
+++ src/main/java/com/example/finance/HelloController.java
@@ -3,6 +3,7 @@ package com.example.finance;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -17,6 +18,20 @@ public class HelloController {
 
     String file = "src/main/java/com/example/finance/Expenses.csv";
     String IncomeFile = "src/main/java/com/example/finance/Income.csv";
+
+    String sumMoney(String f){
+        Double sum = 0.0;
+        for(Categor c: readDateCSV(f)){
+            sum += c.getMoney();
+        }
+        return "" + sum;
+    }
+
+    @FXML
+    private Label eLable;
+
+    @FXML
+    private Label iLable;
 
     @FXML
     private ResourceBundle resources;
@@ -65,22 +80,55 @@ public class HelloController {
     @FXML
     private TextField textIncome;
 
+    ObservableList<PieChart.Data> eDatePieChart = FXCollections.observableArrayList();
+    ObservableList<PieChart.Data> iDatePieChart = FXCollections.observableArrayList();
+
+    void setDateE(){
+        for(Categor c : readDateCSV(file)){
+            eDatePieChart.add(new PieChart.Data(c.getName(), c.getMoney()));
+        }
+    }
+
+    void setDateI(){
+        for(Categor c : readDateCSV(IncomeFile)){
+            iDatePieChart.add(new PieChart.Data(c.getName(), c.getMoney()));
+        }
+    }
+
+
     @FXML
     private PieChart E;
 
     @FXML
     private PieChart I;
 
+    ObservableList<Categor> refreshE(){
+        listExpenses.clear();
+        listExpenses = FXCollections.observableArrayList(readDateCSV(file));
+        return listExpenses;
+    }
+    ObservableList<Categor> refreshI(){
+        listIncome.clear();
+        listIncome = FXCollections.observableArrayList(readDateCSV(IncomeFile));
+        return listIncome;
+    }
+
     @FXML
     void initialize() {
+
+        eLable.setText(sumMoney(file));
+        iLable.setText(sumMoney(IncomeFile));
+
         category_expenses.setItems(langs);
         ButtonExpenses.setOnAction(event -> {
             writeDateCSV(file, new Categor(category_expenses.getValue(), Double.parseDouble(textExpenses.getText())));
+            tableExpenses.setItems(refreshE());
         });
 
         categoryIncome.setItems(list);
         ButtonIncome.setOnAction(event -> {
             writeDateCSV(IncomeFile, new Categor(categoryIncome.getValue(), Double.parseDouble(textIncome.getText())));
+            tableIncome.setItems(refreshI());
         });
 
         category_expenses_tabl.setCellValueFactory(new PropertyValueFactory<Categor, String>("name"));
@@ -90,6 +138,12 @@ public class HelloController {
         category_income_tabl.setCellValueFactory(new PropertyValueFactory<Categor, String>("name"));
         addmon_income_tabl.setCellValueFactory(new PropertyValueFactory<Categor, Double>("money"));
         tableIncome.setItems(listIncome);
+
+        setDateE();
+        E.setData(eDatePieChart);
+
+        setDateI();
+        I.setData(iDatePieChart);
     }
 
 }
